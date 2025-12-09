f4e53b5a95d145200c9aac0ccbeeb947f61c8440
akatz-ai <katzfeyalex+akatzgh@gmail.com>
2025-12-08 22:31:07 -0800

    Bump version

--- js/comfygit-panel.js
+++ js/comfygit-panel.js
@@ -20166,7 +20166,7 @@ const RS = { class: "settings-content" }, MS = { class: "settings-section" }, DS
     ariaLabel: "View ComfyGit Documentation",
     iconPath: "M8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"
   }
-], I6 = "v0.0.8", E6 = "Akatz", T6 = { class: "social-buttons" }, P6 = ["title", "aria-label", "onClick"], R6 = {
+], I6 = "v0.0.9", E6 = "Akatz", T6 = { class: "social-buttons" }, P6 = ["title", "aria-label", "onClick"], R6 = {
   width: "14",
   height: "14",
   viewBox: "0 0 16 16",
